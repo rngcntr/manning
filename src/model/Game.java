37238d6a1e9ebe3dec1aa56da61dee18af6d03ff
Florian Grieskamp <f.grieskamp@gmail.com>
2018-08-15 16:57:10 +0200

    Show timeouts, poss. team, yardline, down, togo and clock

--- src/model/Game.java
+++ src/model/Game.java
@@ -4,33 +4,94 @@ import org.json.simple.*;
 
 public class Game {
 
-	private long homeScore = 0;
-	private long guestScore = 0;
+	private long homeScore = -1L;
+	private long guestScore = -1L;
 	
 	private String homeShort = "";
 	private String guestShort = "";
 
-	private long id = 0L;
-	private String state = "";
+    private long homeTimeouts = 3L;
+    private long guestTimeouts = 3L;
 
-	private boolean redzone;
+    private long down = 0L;
+    private long togo = 0L;
+    private long quarter = 0L;
+    private String yardline = "";
+    private String clock = "";
+
+    private String posTeam = "";
+
+	private long id = 0L;
+	private String state = "Pre";
+
+	private boolean redzone = false;
 
 	public static Game fromJSON (long id, JSONObject jsonObject) {
 		Game output = new Game();
 
 		try {
 			output.id = id;
-			output.state = (String) jsonObject.get("qtr");
 
-            output.homeScore = (long) ((JSONObject) ((JSONObject) jsonObject.get("home")).get("score")).get("T");
-            output.guestScore = (long) ((JSONObject) ((JSONObject) jsonObject.get("away")).get("score")).get("T");
+            Object stateObject = jsonObject.get("qtr");
+            if (stateObject != null) {
+                output.state = (String) stateObject;
+            }
+
+            Object posTeamObject = jsonObject.get("posteam");
+            if (posTeamObject != null) {
+                output.posTeam = (String) posTeamObject;
+            }
 
 			output.homeShort = (String) ((JSONObject) jsonObject.get("home")).get("abbr");
 			output.guestShort = (String) ((JSONObject) jsonObject.get("away")).get("abbr");
+
+            Object homeScoreObject = ((JSONObject) ((JSONObject) jsonObject.get("home")).get("score")).get("T");
+            if (homeScoreObject != null) {
+                output.homeScore = (long) homeScoreObject;
+            }
+
+            Object guestScoreObject = ((JSONObject) ((JSONObject) jsonObject.get("away")).get("score")).get("T");
+            if (guestScoreObject != null) {
+                output.guestScore = (long) guestScoreObject;
+            }
+
+            Object homeTimeoutsObject = ((JSONObject) jsonObject.get("away")).get("to");
+            if (homeTimeoutsObject != null) {
+                output.homeTimeouts = (long) homeTimeoutsObject;
+            }
+
+            Object guestTimeoutsObject = ((JSONObject) jsonObject.get("away")).get("to");
+            if (guestTimeoutsObject != null) {
+                output.guestTimeouts = (long) guestTimeoutsObject;
+            }
+
+            Object downObject = jsonObject.get("down");
+            if (downObject != null) {
+                output.down = (long) downObject;;
+            }
+
+            Object togoObject = jsonObject.get("togo");
+            if (togoObject != null) {
+                output.togo = (long) togoObject;
+            }
+
+            Object quarterObject = jsonObject.get("quarter");
+            if (quarterObject != null) {
+                output.quarter = (long) quarterObject;
+            }
+
+            Object yardlineObject = jsonObject.get("yl");
+            if (yardlineObject != null) {
+                output.yardline = (String) yardlineObject;
+            }
+
+            Object clockObject = jsonObject.get("clock");
+            if (clockObject != null) {
+                output.clock = (String) clockObject;
+            }
 			
-			output.redzone = (boolean) jsonObject.get("redzone");
-	        //  "gsis": 56933,
-	        //  "rz": -1,
+            Object redzoneObject = jsonObject.get("redzone");
+			output.redzone = redzoneObject != null && (boolean) jsonObject.get("redzone");
 		} catch (NullPointerException npex) {
 			System.err.println("Unable to parse Game from JSON");
 			return null;
@@ -73,20 +134,51 @@ public class Game {
 
 	public String toString () {
 		StringBuilder scoreBox = new StringBuilder();
+
+        String homeScoreString = homeScore >= 0 ? String.valueOf(homeScore) : "";
+        String guestScoreString = guestScore >= 0 ? String.valueOf(guestScore) : "";
+
+        String homeShortString = String.format("%-3s", homeShort);
+        if (homeShort.equals(posTeam)) {
+            homeShortString = Printer.decorate(homeShortString, Printer.ANSI_WHITE_BOLD, generateModifiers());
+        }
+
+        String guestShortString = String.format("%3s", guestShort);
+        if (guestShort.equals(posTeam)) {
+            guestShortString = Printer.decorate(guestShortString, Printer.ANSI_WHITE_BOLD, generateModifiers());
+        }
+
 		if(isRunning()) {
 			scoreBox.append("╔═════╤═══════════╤═════╗\n");
-			scoreBox.append(String.format("║ %3d │ %3s @ %-3s │ %-3d ║\n",
-				guestScore, guestShort, homeShort, homeScore));
-			scoreBox.append("╚═════╧═══════════╧═════╝\n");
+			scoreBox.append(String.format("║ %3s │ %s @ %s │ %-3s ║\n",
+				guestScoreString, guestShortString, homeShortString, homeScoreString));
+			scoreBox.append(String.format("╟─────┴─%s───%s─┴─────╢\n", makeTimeoutsString(homeTimeouts),makeTimeoutsString(guestTimeouts)));
+            scoreBox.append(String.format("║ %s&%-2d  %6s  %5s ║\n", Printer.numberAsString(down), togo, yardline, clock));
+			scoreBox.append("╚═══════════════════════╝\n");
 		} else {
 			scoreBox.append("┌─────┬───────────┬─────┐\n");
-			scoreBox.append(String.format("│ %3d │ %3s @ %-3s │ %-3d │\n",
-				guestScore, guestShort, homeShort, homeScore));
-			scoreBox.append("└─────┴───────────┴─────┘\n");
+			scoreBox.append(String.format("│ %3s │ %s @ %s │ %-3s │\n",
+				guestScoreString, guestShortString, homeShortString, homeScoreString));
+			scoreBox.append("├─────┴───────────┴─────┤\n");
+            scoreBox.append("│                       │\n");
+			scoreBox.append("└───────────────────────┘\n");
 		}
 
 		return Printer.decorate(scoreBox.toString(), generateModifiers());
 	}
+
+    private String makeTimeoutsString (long timeouts) {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < 3; ++i) {
+            if (i < timeouts) {
+                output.append("●");
+            } else {
+                output.append("○");
+            }
+        }
+
+        return output.toString();
+    }
 
 	private String generateModifiers () {
 		StringBuilder modifiers = new StringBuilder();
@@ -94,7 +186,7 @@ public class Game {
 		if (redzone) {
 			modifiers.append(Printer.ANSI_RED_BOLD);
 		} else if (isRunning()) {
-			modifiers.append(Printer.ANSI_WHITE_BOLD);
+			modifiers.append(Printer.ANSI_WHITE);
 		}
 
 		return modifiers.toString();
